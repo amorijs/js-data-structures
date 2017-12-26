2d4aa6c9a2d244b407590caa432860ed7c703847
chris <chris.amori93@gmail.com>
2017-12-26 11:25:39 -0800

    Add tests for peek and peekFront

--- test/basic/stack.js
+++ test/basic/stack.js
@@ -54,7 +54,7 @@ describe('stack', () => {
       });
 
       describe('pop', () => {
-        beforeEach(() => {});
+        it('should be a function', () => expect(stack.push).to.be.a('function'));
 
         it('should decrease in length everytime a value is popped', () => {
           const length = 10;
@@ -79,7 +79,7 @@ describe('stack', () => {
           }
         });
 
-        it('should return the top of the stack', () => {
+        it('should return the top of the stack if it has items', () => {
           const length = 10;
 
           for (let i = 0; i < length; i += 1) {
@@ -89,6 +89,40 @@ describe('stack', () => {
           for (let i = length - 1; i >= 0; i -= 1) {
             expect(stack.pop()).to.equal(i);
           }
+        });
+      });
+
+      describe('peek', () => {
+        it('should be a function', () => expect(stack.peek).to.be.a('function'));
+
+        it('should return undefined if the stack is empty', () => {
+          hooks.clearStack(stack);
+          expect(stack.peek()).to.equal(undefined);
+        });
+
+        it('should return the top of the stack if it has items', () => {
+          stack.push(0);
+          expect(stack.peek()).to.equal(0);
+          const uniqueValue = {};
+          stack.push(uniqueValue);
+          expect(stack.peek()).to.equal(uniqueValue);
+        });
+      });
+
+      describe('peekFront', () => {
+        it('should be a function', () => expect(stack.peek).to.be.a('function'));
+
+        it('should return undefined if the stack is empty', () => {
+          hooks.clearStack(stack);
+          expect(stack.peekFront()).to.equal(undefined);
+        });
+
+        it('should return the bottom of the stack if it has items', () => {
+          const uniqueValue = {};
+          stack.push(uniqueValue);
+          expect(stack.peekFront()).to.equal(uniqueValue);
+          stack.push(0);
+          expect(stack.peekFront()).to.equal(uniqueValue);
         });
       });
     });
